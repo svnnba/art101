929710abe1a31857424fa2ab61f127f1b8391a9d
svnnba <nimomohamoud18@gmail.com>
2024-10-21 21:37:29 -0700

    WE DID IT

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - LAB 5
+// Author: Seven.M
+// Date: 10/21/2024
 
-// Constants
 
-// Functions
+//  Define Variables
+make ="Mini";
+model = "Mini Cooper S";
+color = "Chili Red";
+year = 2003;
+
+// calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + model + "<br>");
+document.writeln("Make: " + color + "<br>");
+document.writeln("Make: " + year + "<br>");
+document.writeln("Make: " + age + "<br>");
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
