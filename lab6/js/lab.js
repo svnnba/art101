e34054ad68d4682e5b84be9353985e6f4db35467
svnnba <nimomohamoud18@gmail.com>
2024-10-29 15:37:16 -0700

    yipie

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -6,16 +6,20 @@
 
 // Functions
 
+myTransport = ["Mini Cooper S", "skateboard", "walking", "rides from friends"];
+
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+myMainRide = {
+  make: "Mini",
+  model: "Mini Cooper S",
+  color: "Chili Red",
+  year: 2003,
+  age: function(){
+      return 2024 - this.year; }}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+      document.writeln("Kinds of transport I use: ", myTransport, "</br>")
+
+      document.writeln("My Main Ride: <pre>",
+        JSON.stringify(myMainRide, null, '\t'), "</pre>" );
