12e9a62d294f0635dec129a60a3985a0488812e7
svnnba <nimomohamoud18@gmail.com>
2024-11-26 22:06:00 -0800

    g

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -5,26 +5,37 @@
 // Constants
 
 // Functions
+$(document).ready(function() {
+  // Event listener for the button click
+  $("#submit").click(function() {
+    let oneLongString = ''; // String to hold all the output
 
-function sortingHat(str){
-len = str.length;
-mod = len % 4;
-if (mod == 0){
-  return "Professor Utonium"
-}
-else if (mod == 1){
-  return "Bubbles"
-}
-else if (mod == 2){
-  return "Buttercup"
-}
-else if (mod == 3 ){
-  return "Blossom"
-}
-}
+    // Loop through numbers 1 to 200
+    for (let i = 1; i <= 300; i++) {
 
-$("#button").click(function(){
-  var name = $("input").val()
-  var house = sortingHat(name);
-  $("#output").html("<h1>" + house + "</h1>");
-})
+      if (i % 105 === 0) {
+        debugger
+        oneLongString += `<div class="fizzbuzz-item">FizzBuzzCrash</div>`; // FizzBuzz for multiples of 105
+      }else if (i % 21 === 0) {
+        oneLongString += <div class="fizzbuzz-item">FizzCrash</div>; // Fizz for multiples of 21
+      }else if (i % 35 === 0) {
+        oneLongString += <div class="fizzbuzz-item">BuzzCrash</div>; // Fizz for multiples of 35
+      }else if (i % 15 === 0) {
+        oneLongString += <div class="fizzbuzz-item">FizzBuzz</div>; // Fizz for multiples of 15
+      }else if (i % 3 === 0) {
+        oneLongString += <div class="fizzbuzz-item">Fizz</div>; // Fizz for multiples of 3
+      } else if (i % 5 === 0) {
+        debugger
+        oneLongString += <div class="fizzbuzz-item">Buzz</div>; // Buzz for multiples of 5
+      } else if (i % 7 === 0) {
+        oneLongString += <div class="fizzbuzz-item">Crash</div>; // Crash for multiples of 7
+      } else {
+        oneLongString += <div class="fizzbuzz-item">${i}</div>; // Regular number if not a multiple of the above
+      }
+    }
+
+    // Append the long string with FizzBuzz results to the #output div
+    $("#output").html(oneLongString);
+    $(this).toggleClass("fizzbuzz-item");
+  });
+});
