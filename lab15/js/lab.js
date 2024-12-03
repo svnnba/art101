67f6a5050e56bf1045259687daa609f5477797bc
svnnba <nimomohamoud18@gmail.com>
2024-12-02 21:09:47 -0800

    YES

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -6,20 +6,17 @@
 
 // Functions
 
-myTransport = ["Mini Cooper S", "skateboard", "walking", "rides from friends"];
-
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-myMainRide = {
-  make: "Mini",
-  model: "Mini Cooper S",
-  color: "Chili Red",
-  year: 2003,
-  age: function(){
-      return 2024 - this.year; }}
-
-
-      document.writeln("Kinds of transport I use: ", myTransport, "</br>")
-
-      document.writeln("My Main Ride: <pre>",
-        JSON.stringify(myMainRide, null, '\t'), "</pre>" );
+function getYesNoResponse() {
+  // Fetch the data from the YesNo API
+  fetch('https://yesno.wtf/api')
+      .then(response => response.json())
+      .then(data => {
+          // Display the response and the image
+          document.getElementById('response').innerText = data.answer.charAt(0).toUpperCase() + data.answer.slice(1);
+          document.getElementById('yesnoImage').src = data.image;
+      })
+      .catch(error => {
+          console.error('Error:', error);
+          document.getElementById('response').innerText = 'Sorry, something went wrong.';
+      });
+}
--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -6,20 +6,26 @@
 
 // Functions
 
-myTransport = ["Mini Cooper S", "skateboard", "walking", "rides from friends"];
+let endpoint = "https://xkcd.com/info.0.json";
 
+// Configure the AJAX request
+let ajaxConfig = {
+  url: endpoint, // API URL
+  method: "GET", // HTTP method
+  dataType: "json", // Payload type
+  success: function(data) { // Success handler
+    console.log(data);
+    // Since data is a single object, not an array, access its properties directly
+    let comic = data; 
+    // Append image and alt text
+    $("#output").append("<img src='" + graphic.img + "' alt='" + graphic.alt + "'>");
+    $("#output").append("<p>" + graphic.alt + "</p>");
+  }, 
+  error: function(xhr, status, error) { // Error handler
+    console.error(error); 
+  } 
+};
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-myMainRide = {
-  make: "Mini",
-  model: "Mini Cooper S",
-  color: "Chili Red",
-  year: 2003,
-  age: function(){
-      return 2024 - this.year; }}
+// Send the AJAX request
+$.ajax(ajaxConfig);
 
-
-      document.writeln("Kinds of transport I use: ", myTransport, "</br>")
-
-      document.writeln("My Main Ride: <pre>",
-        JSON.stringify(myMainRide, null, '\t'), "</pre>" );
